b76b4acca04218b73b76458e6ee49640e9f30927
mike_home <grossstadtlegende@gmail.com>
2014-05-23 18:44:34 +0200

    import from first 2 papers -orgueil missing

--- import_from_pub.py
+++ import_from_pub.py
@@ -1,4 +1,5 @@
 __author__ = 'mike'
+from add import new_sample, new_meteorite
 
 
 def Butler1972():
@@ -37,10 +38,9 @@ def Banerjee1972():
     doi:10.1016/0012-821X(72)90265-8
     '''
     allende = new_meteorite(name='allende', group='CV', subgroup='CV3', iron_content='23.85e-2',
-                            magnetic_carriers='95 wt% taenite (67% Ni), 5 wt% taenite (36% Ni)'
-    fall = True, fall_date = '1969 February 08', shock_stage = 'S1')
-    murchison = new_meteorite(name='murchison', group='CM', subgroup='CM2', iron_content='22.13e-2',
-                              fall=True, fall_date='1969 September 28', shock_stage='S1-S2')
+                            magnetic_carriers='95 wt% taenite (67% Ni), 5 wt% taenite (36% Ni)',
+                            fall=True, fall_date='1969 February 08', shock_stage='S1')
+
     Banerjee1972_allende_1a = new_sample(name='allende(a)', meteorite=allende,
                                          intensity=1.23, unit='Oe',
                                          determination_type='TT',
@@ -51,8 +51,43 @@ def Banerjee1972():
                                          determination_type='TT-modified',
                                          citekey='Banerjee1972',
                                          vacuum=2e-5, vac_unit='torr', fit_t_min=20, fit_t_max=110, lab_field=0.52)
+
     Banerjee1972_allende_2a = new_sample(name='allende2(a)', meteorite=allende,
-                                         intensity=0.96, unit='Oe',
+                                         intensity=1.11, unit='Oe',
                                          determination_type='TT-modified',
                                          citekey='Banerjee1972',
-                                         vacuum=2e-5, vac_unit='torr', fit_t_min=20, fit_t_max=110, lab_field=0.52)
+                                         vacuum=2e-5, vac_unit='torr', fit_t_min=20, fit_t_max=130, lab_field=1.04)
+    Banerjee1972_allende_2b = new_sample(name='allende2(b)', meteorite=allende,
+                                         intensity=1.06, unit='Oe',
+                                         determination_type='TT-modified',
+                                         citekey='Banerjee1972',
+                                         vacuum=2e-5, vac_unit='torr', fit_t_min=20, fit_t_max=130, lab_field=1.04)
+
+    murchison = new_meteorite(name='murchison', group='CM', subgroup='CM2', iron_content='22.13e-2',
+                              fall=True, fall_date='1969 September 28', shock_stage='S1-S2')
+
+    Banerjee1972_murchison_1a1 = new_sample(name='murchison1(a)', meteorite=murchison,
+                                            intensity=0.17, unit='Oe',
+                                            determination_type='TT',
+                                            citekey='Banerjee1972',
+                                            vacuum=2e-5, vac_unit='torr', fit_t_min=20, fit_t_max=90, lab_field=0.52)
+    Banerjee1972_murchison_1a2 = new_sample(name='murchison1(a)', meteorite=murchison,
+                                            intensity=0.02, unit='Oe',
+                                            determination_type='TT',
+                                            citekey='Banerjee1972',
+                                            vacuum=2e-5, vac_unit='torr', fit_t_min=90, fit_t_max=300, lab_field=0.52)
+
+    Banerjee1972_murchison_1b1 = new_sample(name='murchison1(b)', meteorite=murchison,
+                                            intensity=0.19, unit='Oe',
+                                            determination_type='TT',
+                                            citekey='Banerjee1972',
+                                            vacuum=2e-5, vac_unit='torr', fit_t_min=20, fit_t_max=90, lab_field=0.52)
+    Banerjee1972_murchison_1b2 = new_sample(name='murchison1(b)', meteorite=murchison,
+                                            intensity=0.01, unit='Oe',
+                                            determination_type='TT',
+                                            citekey='Banerjee1972',
+                                            vacuum=2e-5, vac_unit='torr', fit_t_min=90, fit_t_max=300, lab_field=0.52)
+
+    orgueil = new_meteorite(name='orgueil', group='CI', subgroup='CI1',
+                              fall=True, fall_date='1864 May 14', shock_stage='S1')
+    #todo rest of orgueil meteorite
